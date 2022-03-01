f4dd51388490a839e4cc34b303035e0749dd9caa
jpasikainen <asikainen.juhapekka@gmail.com>
2022-03-01 10:44:03 +0200

    mention source

---
+++ src/RotatingShape.mjs
@@ -0,0 +1,43 @@
+export class RotatingShape {
+  shape;
+  width;
+  height;
+
+  constructor(shape) {
+    if (!Array.isArray(shape)) {
+      shape = shape.split(" ").join("");
+      this.shape = shape.split("\n").map((row) => row.split(""));
+    } else {
+      this.shape = shape;
+    }
+    this.width = this.shape[0].length;
+    this.height = this.shape.length;
+    Object.freeze(this.shape);
+  }
+
+  // source https://stackoverflow.com/questions/15170942/how-to-rotate-a-matrix-in-an-array-in-javascript
+  rotateRight() {
+    const rotatedShape = this.shape;
+    return new RotatingShape(
+        rotatedShape[0].map((val, index) => rotatedShape.map(row => row[index]).reverse())
+      )
+  }
+
+  rotateLeft() {
+    const rotatedShape = this.shape;
+    return new RotatingShape(
+        rotatedShape[0].map((val, index) => rotatedShape.map(row => row[row.length - 1 - index]))
+      )
+  }
+
+  toString() {
+    let drawn = "";
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        drawn += this.shape[i][j].toString();
+      }
+      drawn += "\n";
+    }
+    return drawn;
+  }
+}
