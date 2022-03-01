5c49a5a09a88e96270e14cad4363a32b1f95bdce
jpasikainen <asikainen.juhapekka@gmail.com>
2022-03-01 10:12:48 +0200

    fix logic

--- src/Board.mjs
+++ src/Board.mjs
@@ -4,6 +4,7 @@ export class Board {
   board;
   boardOnSamePosition;
   falling;
+  prevBoard;
 
   constructor(width, height) {
     this.width = width;
@@ -11,6 +12,7 @@ export class Board {
     this.board = Array.from(Array(this.height), () => new Array(this.width).fill("."));
     this.boardOnSamePosition = 0;
     this.falling = false;
+    this.prevBoard = null;
   }
 
   hasFalling() {
@@ -27,7 +29,6 @@ export class Board {
   }
 
   tick() {
-    const prevBoard = this.board;
     for (let i = 0; i < this.width; i++) {
       for (let j = this.height - 1; j > 0; j--) {
         if (this.board[j][i] === "." && this.board[j-1][i] !== ".") {
@@ -36,12 +37,13 @@ export class Board {
         }
       }
     }
-    if (prevBoard === this.board) {
+    if (this.prevBoard && JSON.stringify(this.prevBoard) === JSON.stringify(this.board)) {
+      this.boardOnSamePosition += 1;
       if (this.boardOnSamePosition === 2) {
         this.falling = false;
       }
-      this.boardOnSamePosition += 1;
     }
+    this.prevBoard = this.board;
   }
 
   toString() {
