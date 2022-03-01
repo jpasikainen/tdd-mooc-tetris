a77abbbb77f296dbee4fb14afe88d7d0f4b47705
jpasikainen <asikainen.juhapekka@gmail.com>
2022-03-01 10:02:51 +0200

    improve implementationm

--- src/Board.mjs
+++ src/Board.mjs
@@ -2,24 +2,32 @@ export class Board {
   width;
   height;
   board;
-  fallTicks;
+  boardOnSamePosition;
+  falling;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
     this.board = Array.from(Array(this.height), () => new Array(this.width).fill("."));
-    this.fallTicks = 0;
+    this.boardOnSamePosition = 0;
+    this.falling = false;
+  }
+
+  hasFalling() {
+    return this.falling;
   }
 
   drop(block) {
-    if (this.fallTicks !== 0) {
+    if (this.hasFalling()) {
       throw "already falling";
     }
-    this.fallTicks += 1;
+    this.falling = true;
+    this.boardOnSamePosition = 0;
     this.board[0][Math.floor(this.width / 2)] = block.color;
   }
 
   tick() {
+    const prevBoard = this.board;
     for (let i = 0; i < this.width; i++) {
       for (let j = this.height - 1; j > 0; j--) {
         if (this.board[j][i] === "." && this.board[j-1][i] !== ".") {
@@ -28,14 +36,12 @@ export class Board {
         }
       }
     }
-    this.fallTicks += 1;
-    if (this.fallTicks === this.height) {
-      this.fallTicks = 0;
+    if (prevBoard === this.board) {
+      if (this.boardOnSamePosition === 2) {
+        this.falling = false;
+      }
+      this.boardOnSamePosition += 1;
     }
-  }
-
-  hasFalling() {
-    return this.fallTicks === 0;
   }
 
   toString() {
