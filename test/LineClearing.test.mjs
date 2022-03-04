a737acf54ae731479607d4fc6572186dd3da68e4
jpasikainen <asikainen.juhapekka@gmail.com>
2022-03-04 18:23:32 +0200

    create test

--- test/LineClearing.test.mjs
+++ test/LineClearing.test.mjs
@@ -44,4 +44,30 @@ describe("Line clears when", () => {
        OO.T.`
     );
   });
+  it("full line and drop blocks above down", () => {
+    board = new Board(5,4);
+    board.drop(Tetromino.O_SHAPE[0]);
+    board.moveLeft();
+    board.moveDown();
+    board.moveDown();
+    board.tick();
+    board.drop(Tetromino.T_SHAPE[2]);
+    board.moveRight();
+    board.moveRight();
+    board.moveDown();
+    board.moveDown();
+    expect(board.toString()).to.equalShape(
+      `.....
+       .....
+       OO.T.
+       OOTTT`
+    );
+    board.tick();
+    expect(board.toString()).to.equalShape(
+      `.....
+       .....
+       .....
+       OO.T.`
+    );
+  });
 });
