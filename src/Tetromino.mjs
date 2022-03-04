b37c53944f881b0031ad085284a6c3fbaacb8de6
jpasikainen <asikainen.juhapekka@gmail.com>
2022-03-04 17:46:11 +0200

    Fix tetromino

--- src/Tetromino.mjs
+++ src/Tetromino.mjs
@@ -2,9 +2,24 @@ import { RotatingShape } from "./RotatingShape.mjs";
 import { Shapes } from "./Shapes.mjs";
 
 export class Tetromino {
-  static T_SHAPE = new RotatingShape(Shapes.T_SHAPE);
+  static T_SHAPE = [
+    new RotatingShape(Shapes.T_SHAPE[0]),
+    new RotatingShape(Shapes.T_SHAPE[1]),
+    new RotatingShape(Shapes.T_SHAPE[2]),
+    new RotatingShape(Shapes.T_SHAPE[3]),
+  ];
 
-  static I_SHAPE = new RotatingShape(Shapes.I_SHAPE[0]);
+  static I_SHAPE = [
+    new RotatingShape(Shapes.I_SHAPE[0]),
+    new RotatingShape(Shapes.I_SHAPE[1]),
+    new RotatingShape(Shapes.I_SHAPE[2]),
+    new RotatingShape(Shapes.I_SHAPE[3])
+  ];
 
-  static O_SHAPE = new RotatingShape(Shapes.O_SHAPE);
+  static O_SHAPE = [
+    new RotatingShape(Shapes.O_SHAPE[0]),
+    new RotatingShape(Shapes.O_SHAPE[1]),
+    new RotatingShape(Shapes.O_SHAPE[2]),
+    new RotatingShape(Shapes.O_SHAPE[3]),
+  ];
 }
