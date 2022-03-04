6f72b2cfd44dfc5e885ab424bebfcbc3cd234998
jpasikainen <asikainen.juhapekka@gmail.com>
2022-03-04 19:49:07 +0200

    add test scoring

---
+++ test/TestScoring.test.mjs
@@ -0,0 +1,20 @@
+import { expect } from "chai";
+import { Board } from "../src/Board.mjs";
+import { Tetromino } from "../src/Tetromino.mjs";
+import { Score } from "../src/Score.mjs";
+
+describe("Score when", () => {
+  let score;
+  beforeEach(() => {
+    score = new Score();
+  });
+  it("line is cleared", () => {
+    score.linesCleared(1);
+    expect(score.score).to.equal(40)
+  });
+  it("multiple lines are cleared", () => {
+    score.linesCleared(1);
+    score.linesCleared(4);
+    expect(score.score).to.equal(3040)
+  });
+});
