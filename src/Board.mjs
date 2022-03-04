1231b5ca99fd1a12041d3e016a1dc28b357f35e7
jpasikainen <asikainen.juhapekka@gmail.com>
2022-03-04 12:21:53 +0200

    small refactor

--- src/Board.mjs
+++ src/Board.mjs
@@ -109,36 +109,13 @@ export class Board {
   }
 
   rotateLeft() {
-    const prevBoard = this.board;
-    this.draw();
     this.fallingBlock = this.fallingBlock.rotateLeft();
-    for (let i = 0; i < this.fallingBlock.height; i++) {
-      for (let j = 0; j < this.fallingBlock.width; j++) {
-        if (this.fallingBlock.shape[i][j] !== "." && this.fallingBlockPos[1]+j > 0 && this.fallingBlockPos[1]+j < this.width) {
-          this.board[this.fallingBlockPos[0]+i-1][this.fallingBlockPos[1]+j] = this.fallingBlock.shape[i][j];
-        } else if (j + this.fallingBlockPos[1] >= this.width || this.landed[this.fallingBlockPos[0]+i-1][this.fallingBlockPos[1]+j] !== ".") {
-          this.board = prevBoard;
-          if (this.board[i][j])
-          return;
-        }
-      }
-    }
+    this.moveBlock();
   }
 
   rotateRight() {
-    const prevBoard = this.board;
-    this.draw();
     this.fallingBlock = this.fallingBlock.rotateRight();
-    for (let i = 0; i < this.fallingBlock.height; i++) {
-      for (let j = 0; j < this.fallingBlock.width; j++) {
-        if (this.fallingBlock.shape[i][j] !== "." && this.fallingBlockPos[1]+j > 0 && this.fallingBlockPos[1]+j < this.width) {
-          this.board[this.fallingBlockPos[0]+i-1][this.fallingBlockPos[1]+j] = this.fallingBlock.shape[i][j];
-        } else if (j + this.fallingBlockPos[1] >= this.width || this.landed[this.fallingBlockPos[0]+i-1][this.fallingBlockPos[1]+j] !== ".") {
-          this.board = prevBoard;
-          return;
-        }
-      }
-    }
+    this.moveBlock();
   }
 
   toString() {
