cfc773b18ccba6263e64f8b8c8ca59efad0d1463
jpasikainen <asikainen.juhapekka@gmail.com>
2022-03-05 17:17:28 +0200

    add test

--- test/Randomizer.test.mjs
+++ test/Randomizer.test.mjs
@@ -22,9 +22,15 @@ describe("Shuffle bag", () => {
     randomizer.next();
     expect(randomizer.next()).to.equal(Tetromino.I_SHAPE);
   })
-  it("returns I shape most likely", () => {
+  it("returns I shape most likely when it's dominant", () => {
     randomizer.add(Tetromino.T_SHAPE, 1);
     randomizer.add(Tetromino.I_SHAPE, 100000);
     expect(randomizer.next()).to.equal(Tetromino.I_SHAPE);
+  })
+  it("returns T shape most likely when it's dominant after 1000 picks", () => {
+    randomizer.add(Tetromino.I_SHAPE, 1);
+    randomizer.add(Tetromino.T_SHAPE, 100000);
+    for (let i = 0; i < 1000; i++) randomizer.next();
+    expect(randomizer.next()).to.equal(Tetromino.T_SHAPE);
   })
 });
