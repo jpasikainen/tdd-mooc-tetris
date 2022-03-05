40be27bc324f27c649109c2a9f7db96ca0821499
jpasikainen <asikainen.juhapekka@gmail.com>
2022-03-05 16:53:46 +0200

    shuffle bag test

---
+++ test/Randomizer.test.mjs
@@ -0,0 +1,15 @@
+import { expect } from "chai";
+import { Tetromino } from "../src/Tetromino.mjs";
+import { Randomizer } from "../src/Randomizer.mjs";
+
+describe("Shuffle bag", () => {
+  let randomizer;
+  beforeEach(() => {
+    randomizer = new Randomizer();
+  });
+  it("takes tetrominoes", () => {
+    expect(randomizer.data.length).to.equal(0);
+    randomizer.add(Tetromino.I_SHAPE);
+    expect(randomizer.data.length).to.equal(1);
+  })
+});
